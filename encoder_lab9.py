9e696a331e5f57d00bfa536ca38e4d98ce4ccf96
gitttjonzzz <palma.jonathan@ufl.edu>
2024-04-11 12:51:50 -0400

    Update encoder_lab9.py

--- encoder_lab9.py
+++ encoder_lab9.py
@@ -5,6 +5,18 @@ def encoder(password):
         digit = int(i) + 3
         encoded_password += str(digit)
     return encoded_password
+def decode(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        decoded_password += str((int(digit) - 3) % 10)
+    return decoded_password
+
+# Example usage:
+encoded_password = "45678888"
+decoded_password = decode(encoded_password)
+print("Encoded Password:", encoded_password)
+print("Decoded Password:", decoded_password)
+
 
 
 
